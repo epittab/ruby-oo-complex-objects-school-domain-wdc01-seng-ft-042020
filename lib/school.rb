3d8b20a3d79a565089957810480ef64231668321
epittab <ep7m@me.com>
2020-04-22 17:14:30 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+class School
+
+    attr_reader
+    attr_writer
+    attr_accessor :roster, :name
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if (self.roster[grade])
+            self.roster[grade] << name
+        else
+            self.roster[grade] = [name]
+        end
+    end
+
+    def grade(level) 
+        @roster[level]
+    end
+
+    def sort()
+        self.roster.each do |k, v| 
+            v.sort!()
+        end
+    end
+
+end
